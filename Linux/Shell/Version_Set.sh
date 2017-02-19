68cafdf2cb13d0a0dffe73794955839140bfba4c
Ian Hom <ianhom@163.com>
2017-02-19 20:35:30 +0800

    Update Version_Set.sh

--- Linux/Shell/Version_Set.sh
+++ Linux/Shell/Version_Set.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-rm -f version
+rm -f version     #删除旧的verison文件
 
-eche -e "THE HARDWARE VERSION:\n$1" > version
+eche -e "THE HARDWARE VERSION:\n$1" > version  #创建新的version文件，带入版本号信息变量
 
-cat version
+cat version   # 查看version更新的内容
