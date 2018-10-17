4e0ce4955e0fa9efb06dc779b1064c405521064f
A Byte of China <ianhom@163.com>
2018-10-17 11:02:40 +0800

    Update DP2.c

--- Languages/C/DP2.c
+++ Languages/C/DP2.c
@@ -5,11 +5,7 @@ int a[MAX][MAX] = {{1,0,0,0,0},
                      {3,2,1,0,0},
                      {4,3,2,1,0},
                      {5,4,3,2,1}}; 
-int dp[MAX][MAX]= {{0,0,0,0,0},
-                     {0,0,0,0,0},
-                     {0,0,0,0,0},
-                     {0,0,0,0,0},
-                     {0,0,0,0,0}};  
+int dp[MAX]= {0,0,0,0,0};  
                      
 int max(int a, int b)
 {
@@ -25,15 +21,15 @@ int main(void)
 
     for(i=0;i<n;i++)
     {
-        dp[n-1][i]=a[n-1][i];
+        dp[i]=a[n-1][i];
     }
     for(i=n-2;i>=0;i--)   
     {
         for(j=0;j<=i;j++)
         {
-            dp[i][j]=max(dp[i+1][j],dp[i+1][j+1])+a[i][j];
+            dp[j]=max(dp[j],dp[j+1])+a[i][j];
         }
     }
-    printf("%d\n",dp[0][0]);
+    printf("%d\n",dp[0]);
     return 0；
 }
