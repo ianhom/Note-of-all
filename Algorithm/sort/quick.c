605dd110336dcddbf04c5a254cc4ffe15c3ecefd
A Byte of China <ianhom@163.com>
2019-02-15 21:29:36 +0800

    Update quick.c

--- Algorithm/sort/quick.c
+++ Algorithm/sort/quick.c
@@ -7,9 +7,14 @@ void qsort(int *a, int l, int r)
     int i=l,j=r,x=a[i];
     while(i<j)
     {
-        while(i<j && s[j]>=x)
+        while(i<j && a[j]>=x)
             j--;
         s[i]=s[j];
-        
+        while(i<j && a[i]<=x)
+            i++;
+        a[j]=a[i];
     }
+    s[l]=x;
+    qsort(a,l,i-1);
+    qsort(a,i+1,r);
 }
