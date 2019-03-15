4c6eade5ffea4842e57e8e982fb93c5bdededd5d
ianhom <ianhom@163.com>
2019-03-15 08:06:58 +0800

    update

--- Algorithm/practice/countnum.c
+++ Algorithm/practice/countnum.c
@@ -1,4 +1,10 @@
 #include <stdio.h>
+void cal(int a)
+{
+    n[a%10]++;
+    cal(a/10)
+}
+
 
 int n[10]={0};
 void main(int argc, char **argv)
@@ -8,12 +14,16 @@ void main(int argc, char **argv)
     while(*s>='0'&&*s<='9')ret=ret*10+*s++-'0';
     for(int i=1; i<=ret;i++)
     {
+     #if(1)
+        cal(i);
+     #else
         int j=i;      
         do
         {
             n[j%10]++;
             j/=10;
         }while(j);
+     #endif
     }
     for(int i=0;i<10;i++)
         printf("%d,",n[i]);
