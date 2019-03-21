c70a8b2cd9ada7cde0cbbfb766ad41010c0f9ac2
A Byte of China <ianhom@163.com>
2019-03-21 20:38:47 +0800

    Update digit_count.c

--- Algorithm/practice/digit_count.c
+++ Algorithm/practice/digit_count.c
@@ -1,10 +1,10 @@
 //题目：从1到N自然数，将所有数字练起来，统计数字0~9出现的次数。
-//分析：简单做法就是挨个循环统计，，进阶就是摸索规律，最快的方法就是先计算一次n，然后把所有的结果都保存下来，下一次直接查表
+//分析：简单做法就是挨个循环统计，进阶就是摸索规律，最快的方法就是先计算一次n，然后把所有的结果都保存下来，下一次直接查表。
 #include <stdio.h>
 void cal(int a)
 {
-    n[a%10]++;
-    cal(a/10)；
+    n[a%10]++; 
+    cal(a/10);//迭代方法
 }
 
 
@@ -17,7 +17,7 @@ void main(int argc, char **argv)
     for(int i=1; i<=ret;i++)
     {
      #if(1)
-        cal(i);    //same way with while, but slower;
+        cal(i);    //same way with while, but slower, because of function calling;
      #else
         int j=i;      
         do
