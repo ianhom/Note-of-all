11dbc1ffb2f7a84951c43ee0b5932e96b7da5c8b
A Byte of China <ianhom@163.com>
2020-04-27 21:29:53 +0800

    Update Version_Set.sh

--- Linux/Shell/Version_Set.sh
+++ Linux/Shell/Version_Set.sh
@@ -2,6 +2,6 @@
 
 rm -f version     #删除旧的verison文件
 
-eche -e "THE HARDWARE VERSION:\n$1" > version  #创建新的version文件，带入版本号信息变量
+echo -e "THE HARDWARE VERSION:\n$1" > version  #创建新的version文件，带入版本号信息变量
 
 cat version   # 查看version更新的内容
