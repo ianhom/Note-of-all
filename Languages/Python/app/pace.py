d1345e856bf7553e649a7cc826188e0858fc5b86
A Byte of China <ianhom@163.com>
2020-06-25 20:43:48 +0800

    Update pace.py

--- Languages/Python/app/pace.py
+++ Languages/Python/app/pace.py
@@ -7,7 +7,23 @@ def time_int(t_str):
 
 def pace_sec(p_str):
     p = pace_int(p_str)
-    return (p/100*60+p%60)
+    return (p/100*60+p%100)
+
+def time_sec(t_str):
+    t = time_int(t_str)
+    return (p/100*3600+p%100*60)
+
+def cal_pace(d,t_str):
+    t = time_sec(t_str)
+    return (t*1.00/d)
+
+def half_pace(t_str):
+    return cal_pace(21.1,t_str)
+
+def full_pace(t_str):
+    return cal_pace(42.2,t_str)
     
 if __name__ == '__main__':
     pace_sec(400)
+    half_pace(200)
+    full_pace(400)
