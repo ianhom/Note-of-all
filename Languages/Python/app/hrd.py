a52c74d4a91451cc088f97c08d563e559c0c0ce0
A Byte of China <ianhom@163.com>
2020-07-03 21:02:49 +0800

    Update hrd.py

--- Languages/Python/app/hrd.py
+++ Languages/Python/app/hrd.py
@@ -3,3 +3,10 @@ map = [[1,2,3,4],
        [5,6,7,8],
        [9,10,11,12],
        [13,14,15,0]]
+
+def find_empty():
+    for i in range(4):
+        for j in range(4):
+            if map[i][j] == 0:
+                return i,j
+    return -1,-1
