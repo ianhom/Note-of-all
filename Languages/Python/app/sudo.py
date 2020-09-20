59d744c11b03a741968957c5ab693f9a74f2fe9a
A Byte of China <ianhom@163.com>
2020-09-21 06:05:05 +0800

    Update sudo.py

--- Languages/Python/app/sudo.py
+++ Languages/Python/app/sudo.py
@@ -11,3 +11,8 @@ def inc_cnt():
 def reset_cnt():
     global cnt
     cnt = 0
+    
+def create_map():
+    map_l = [[inc_cnt() for i in range(9)] for j in range(9)]
+    reset_cnt()
+    return map_l
