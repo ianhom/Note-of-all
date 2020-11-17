f0fede91294cbf605caee1bbc8b549cbe98da5fb
A Byte of China <ianhom@163.com>
2020-11-18 05:52:49 +0800

    Update heartrate.py

--- Languages/Python/app/heartrate.py
+++ Languages/Python/app/heartrate.py
@@ -1,6 +1,11 @@
 import sys
 import datetime
-age = datetime.datetime().now.year - 1987
+
+if len(sys.argv) == 0:
+    birth = 1987
+    peace_rate = 60
+
+age = datetime.datetime.now().year - birth
 MAX = 220 - age
 Ep  = MAX * 0.6
 Mp  = MAX * 0.75
