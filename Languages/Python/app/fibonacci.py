235dd389db807aa225f2bc7829a8e4b15a55af62
A Byte of China <ianhom@163.com>
2020-12-01 06:09:32 +0800

    Update fibonacci.py

--- Languages/Python/app/fibonacci.py
+++ Languages/Python/app/fibonacci.py
@@ -14,13 +14,13 @@ def cal2(x):
     if a[x] != 0:
         return a[x]
     if x == 1 or x == 2:
-        return 1
+        return
     else:
         a[x-1]=cal2(x-1)
         a[x-2]=cal2(x-2)
         retun a[x-1]+a[x-2]
 
-a=int(sys.argv[1])
+c=int(sys.argv[1])
 
-cal(a)
-cal2(a)
+cal(c)
+cal2(c)
