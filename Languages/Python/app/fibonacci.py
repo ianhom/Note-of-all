05dde73e05e32f9f3b7719e9298997c284bc89bc
A Byte of China <ianhom@163.com>
2021-01-10 17:56:24 +0800

    Update fibonacci.py

--- Languages/Python/app/fibonacci.py
+++ Languages/Python/app/fibonacci.py
@@ -67,17 +67,8 @@ def do_it():
     t2,r2 = timing(cal2,c)
     t3,r3 = timing(cal3,c)
 
-    t0=time.time()
-    r1=cal(c)
-    t1=time.time()-t0
     print("The result is ",r1,"\trunning time is ",t1,"\trunning count is ",cnt1)
-    t0=time.time()
-    r2=cal2(c)
-    t2=time.time()-t0
     print("The result is ",r2,"\trunning time is ",t2,"\trunning count is ",cnt2)
-    t0=time.time()
-    r3=cal3(c)
-    t3=time.time()-t0
     print("The result is ",r3,"\trunning time is ",t3,"\trunning count is ",cnt3)
 
 if __name__ == '__main__':
