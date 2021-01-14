91528f6abad42a004934647febf5b54783bd224b
A Byte of China <ianhom@163.com>
2021-01-15 06:18:26 +0800

    Update vol.py

--- Languages/Python/app/vol.py
+++ Languages/Python/app/vol.py
@@ -2,7 +2,22 @@
 import time
 import sys
 
-days = time.dayofmonth()
+def get_last_day(year, month):
+    flag = False
+    if year%4 == 0 and year%100 != 0:
+        flag = True
+    if year%400 == 0:
+        flag = True
+    if month in [1,3,5,7,8,10,12]:
+        return 31
+    if month in [4,6,9,11]:
+        return 30
+    if month == 2 and flag == True:
+        return 29
+    if month == 2 and flag == False:
+        return 28
+
+dyas = time.dayofmonth()
 vol_total = sys.argv[1]
 vol = int(vol_total) / days
 
