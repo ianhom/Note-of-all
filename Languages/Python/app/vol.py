752c826f160eca1e0e8cb2d28caf9e1ea899271e
A Byte of China <ianhom@163.com>
2021-01-27 22:14:12 +0800

    Update vol.py

--- Languages/Python/app/vol.py
+++ Languages/Python/app/vol.py
@@ -7,19 +7,8 @@ m=datetime.datetime.now().month
 d=datetime.datetime.now().day
 
 def get_last_day(year, month):
-    flag = False
-    if year%4 == 0 and year%100 != 0:
-        flag = True
-    if year%400 == 0:
-        flag = True
-    if month in [1,3,5,7,8,10,12]:
-        return 31
-    if month in [4,6,9,11]:
-        return 30
-    if month == 2 and flag == True:
-        return 29
-    if month == 2 and flag == False:
-        return 28
+    return (datetime.datetime(year.month+1) - datetime.datetime(year.month)).days
+
 
 vol_total = int(input("The object is :"));
 cur_total = int(input("The current is :"));
