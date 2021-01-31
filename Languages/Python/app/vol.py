372cff9076bffce98a1713c53df8117cc441cc10
A Byte of China <ianhom@163.com>
2021-01-31 02:17:33 +0000

    Update vol.py

--- Languages/Python/app/vol.py
+++ Languages/Python/app/vol.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python
 import datetime
 import requests
-import json
 
 r = requests.get('http://www.weather.com.cn/data/sk/101070201.html')
 r.encoding='utf-8'
-print(r.json())
+print(r.json()['weatherinfo']['city'], r.json()['weatherinfo']['temp'])
 
 y=datetime.datetime.now().year
 m=datetime.datetime.now().month
