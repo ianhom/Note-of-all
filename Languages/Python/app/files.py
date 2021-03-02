cc02df69e73c7a5b27e28c05c0ac07cc93071262
ianhom <ianhom@163.com>
2021-03-03 06:17:33 +0800

    update

--- Languages/Python/app/files.py
+++ Languages/Python/app/files.py
@@ -2,7 +2,9 @@
 
 write_in = "Hello world"
 
-with open("./test.txt","w") as f:
+with open("./test.txt","r") as f:
     s=f.read()
     print(s)
+
+with open("./test.txt","w") as f:
     f.write(write_in)
